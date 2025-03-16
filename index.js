eb065bc56250051218058d3bfd6aa073e745449c
FrostSTeeL <frost.steel@gmail.com>
2025-03-16 20:52:57 +0300

    popcorn, food and fries added!

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ var ginAmount = document.getElementById("ginAmount");
 var shotAmount = document.getElementById("shotAmount");
 var waterAmount = document.getElementById("waterAmount");
 var nutsAmount = document.getElementById("nutsAmount");
+var pCornAmount = document.getElementById("pCornAmount");
 var picklesAmount = document.getElementById("picklesAmount");
+var foodAmount = document.getElementById("foodAmount");
+var friesAmount = document.getElementById("friesAmount");
 
 function appendToDrink(drinkName) {
     var newNum = parseInt(drinkName.textContent) + 1;
@@ -28,14 +31,20 @@ function StartFunc() {
         shotAmount.textContent = localStorage.getItem("shotAmount")
         waterAmount.textContent = localStorage.getItem("waterAmount")
         nutsAmount.textContent = localStorage.getItem("nutsAmount")
+        pCornAmount.textContent = localStorage.getItem("pCornAmount")
         picklesAmount.textContent = localStorage.getItem("picklesAmount")
+        foodAmount.textContent = localStorage.getItem("foodAmount")
+        friesAmount.textContent = localStorage.getItem("friesAmount")
     } else {
         localStorage.setItem("beerAmount", "0");
         localStorage.setItem("ginAmount", "0")
         localStorage.setItem("shotAmount", "0")
         localStorage.setItem("waterAmount", "0")
         localStorage.setItem("nutsAmount", "0")
+        localStorage.setItem("pCornAmount", "0")
         localStorage.setItem("picklesAmount", "0")
+        localStorage.setItem("foodAmount", "0")
+        localStorage.setItem("friesAmount", "0")
     }
 }
 
